66fa5e0ff55ee19a73b50b4162807921bcda55d7
santhi1213 <santhiraju32@gmail.com>
2025-09-06 19:41:43 +0530

    added icon and removed additional slash in api url

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -986,7 +986,7 @@ const TasklistPage = () => {
   });
 
   // API base URL
-  const API_BASE_URL = 'https://freelance-backend-0tw4.onrender.com/';
+  const API_BASE_URL = 'https://freelance-backend-0tw4.onrender.com';
 
   // Load projects and tasks based on user role
   const loadData = async () => {
